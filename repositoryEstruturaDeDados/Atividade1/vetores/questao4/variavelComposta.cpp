adf8bc3e4080b2ce40d27b29427b2270a6e07ba9
gersin <gersonfernandesribeiro48@gmail.com>
2024-08-12 22:08:44 -0300

    commit

--- repositoryEstruturaDeDados/Atividade1/vetores/questao4/variavelComposta.cpp
+++ repositoryEstruturaDeDados/Atividade1/vetores/questao4/variavelComposta.cpp
@@ -8,7 +8,8 @@
 //int main()
 //{
 //    int numValores = 0;
-//    int maiorElemento = 0;
+//    int maioresElementos[2] = {0, 0};
+//    int aux = 0;
 //    int somarPares = 0;
 //
 //    cout << "Quantos valores vao ser inseridos (de 0 a 10)?" << endl;
@@ -18,20 +19,36 @@
 //
 //    for (int i = 0; i < numValores; ++i) {
 //        cin >> variavelComposta[i];
-//        if (variavelComposta[i - 1] <= variavelComposta[i])
-//            maiorElemento = variavelComposta[i];
+//        if (i == 0 || i == 1) {
+//            maioresElementos[i] = variavelComposta[i];
+//            if (maioresElementos[0] <= maioresElementos[1]) {
+//                aux = maioresElementos[1];
+//                maioresElementos[1] = maioresElementos[0];
+//                maioresElementos[0] = aux;
+//            }
+//        }
+//        if (maioresElementos[0] <= variavelComposta[i]) {
+//            maioresElementos[0] = variavelComposta[i];
+//        }
+//        else if (maioresElementos[1] <= variavelComposta[i]) {
+//                maioresElementos[1] = variavelComposta[i];
+//        }
 //    }
 //
 //    for (int i = 0; i < numValores; ++i) {
 //        if (i%2 != 0 && i > 0) {
 //            cout << "Posicao impar considerando o zero como posicao inicial: " << i <<
-//            "\n" << "Valor na posicao: " << variavelComposta[i] << endl;
-//            somarPares += variavelComposta[i];
+//            "\t" << "Valor na posicao: " << variavelComposta[i] << endl;
+//            if (variavelComposta[i]%2 == 0) {
+//
+//                somarPares += variavelComposta[i];
+//            }
 //        }
 //    }
 //
-//    cout << "Maior elemento:" << maiorElemento << "\n" <<
-//        "Soma total dos elementos pares nas posicoes impares:" << somarPares << endl;
+//    cout << "Maiores elementos: Primeiro maior: " << maioresElementos[0] << "\t|\tSegundo maior: "
+//    << maioresElementos[1] << "\n" << "Soma total dos elementos pares nas posicoes impares: "
+//    << somarPares << endl;
 //
 //    return 0;
 //}
